33d87d4c5d55e1d0d448d6347ea1e186ff5e6dc6
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2023-12-09 22:38:30 -0500

    added sytling to form, buttons, imported icons, and google font

--- src/ToDo/ToDo.tsx
+++ src/ToDo/ToDo.tsx
@@ -1,9 +1,9 @@
 import { ChangeEvent, useState, MouseEvent } from "react"
-
+import { MdOutlineAddCircleOutline } from "react-icons/md";
+import { GoChecklist } from "react-icons/go";
 
 interface Task {
     todoTask: string
-    
 }
 
 const ToDo = () => {
@@ -12,20 +12,19 @@ const ToDo = () => {
     
     const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
         if (event.target.name === 'task')
-        setTask(event.target.value)
+        setTask(event.target.value);
     }
 
     const addTask = (event: MouseEvent) => {
         event.preventDefault()
-        const newTask = {todoTask: task}
+        const newTask = {todoTask: task};
         setTodoList([...todoList, newTask]);
-        setTask('')
+        setTask('');
     }
 
     const removeTask = (itemToRemove: string) => {
         setTodoList(todoList.filter((item) => {
-            return item.todoTask !== itemToRemove}))
-        
+            return item.todoTask !== itemToRemove}));
     }
 
   return (
@@ -33,21 +32,23 @@ const ToDo = () => {
     <h2 className="funny-header">"Never put off till tomorrow what you can do the day after tomorrow just as well." - Mark Twain</h2>
     <div className="task-container">
         <form className="input-form">
-            <input type="text" placeholder="Add Task" name="task" value={task} onChange={handleChange}/>
-            <button onClick={addTask}>Add</button>
+            <input className="input" type="text" placeholder="Task" name="task" value={task} onChange={handleChange}/>
+
+            <button className="add-btn" onClick={addTask}>Add <MdOutlineAddCircleOutline /></button>
         </form>
     </div>
 
     <div className="list-container">
     {todoList.length > 0 ? (
     todoList.map((item , index) => {
-        return <div key={index}>{item.todoTask}
-        <button onClick={() =>{removeTask(item.todoTask)}}>Remove</button>
+        return <div className="todo-item" key={index}>{item.todoTask}
+
+        <button className="remove-btn" onClick={() =>{removeTask(item.todoTask)}}>Remove <GoChecklist /></button>
         </div>
     })
     ) : (
         <div className="no-task">
-          <h3 className="empty">Nothing To Do Yet!</h3>
+          <h3 className="empty">No Task Yet!</h3>
         </div> )}
     </div>
 </>
