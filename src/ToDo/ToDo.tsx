aceda63b26de120fbafc07da4f108b87a253f42b
adrienner1988 <adriennerdaniels@gmail.com>
2024-09-04 07:54:18 -0400

    Meade it impossible to add a task unless the input had text. Made the header bigger.

--- src/ToDo/ToDo.tsx
+++ src/ToDo/ToDo.tsx
@@ -1,57 +1,90 @@
-import { ChangeEvent, useState, MouseEvent } from "react"
+import { ChangeEvent, useState, MouseEvent } from "react";
 import { MdOutlineAddCircleOutline } from "react-icons/md";
 import { GoChecklist } from "react-icons/go";
 
 interface Task {
-    todoTask: string
+  todoTask: string;
 }
 
 const ToDo = () => {
-    const [task, setTask] = useState<string>('');
-    const [todoList, setTodoList] = useState<Task[]>([]);
-    
-    const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
-        if (event.target.name === 'task')
-        setTask(event.target.value);
+  const [task, setTask] = useState<string>("");
+  const [todoList, setTodoList] = useState<Task[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") setTask(event.target.value);
+  };
+
+  const addTask = (event: MouseEvent) => {
+    event.preventDefault();
+    if (task.trim() === "") {
+      alert("Add a task");
+      return;
     }
 
-    const addTask = (event: MouseEvent) => {
-        event.preventDefault()
-        const newTask = {todoTask: task};
-        setTodoList([...todoList, newTask]);
-        setTask('');
-    }
+    const newTask = { todoTask: task };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+  };
 
-    const removeTask = (itemToRemove: string) => {
-        setTodoList(todoList.filter((item) => {
-            return item.todoTask !== itemToRemove}));
-    }
+  const removeTask = (itemToRemove: string) => {
+    setTodoList(
+      todoList.filter((item) => {
+        return item.todoTask !== itemToRemove;
+      })
+    );
+  };
 
   return (
     <>
-    <h2 className="funny-header">"Never put off till tomorrow what you can do the day after tomorrow just as well." - Mark Twain</h2>
-    <div className="task-container">
-        <form className="input-form">
-            <input className="input" type="text" placeholder="Task" name="task" value={task} onChange={handleChange}/>
+      <div>
+        <h1 className="funny-header">
+          "Never put off till tomorrow what you can do the day after tomorrow
+          just as well." - Mark Twain
+        </h1>
+        <div className="task-container">
+          <form className="input-form">
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter Task..."
+              name="task"
+              value={task}
+              onChange={handleChange}
+            />
 
-            <button className="add-btn" onClick={addTask}>Add <MdOutlineAddCircleOutline /></button>
-        </form>
-    </div>
-
-    <div className="list-container">
-    {todoList.length > 0 ? (
-    todoList.map((item , index) => {
-        return <div className="todo-item" key={index}>{item.todoTask}
-
-        <button className="remove-btn" onClick={() =>{removeTask(item.todoTask)}}>Remove <GoChecklist /></button>
+            <button className="add-btn" onClick={addTask}>
+              Add Task <MdOutlineAddCircleOutline />
+            </button>
+          </form>
         </div>
-    })
-    ) : (
-        <div className="no-task">
-          <h3 className="empty">No Task Yet!</h3>
-        </div> )}
-    </div>
-</>
-  )
-}
-export default ToDo
+
+        <div className="list-container">
+          {todoList.length > 0 ? (
+            todoList.map((item, index) => {
+              return (
+                <div className="todo-item" key={index}>
+                  {item.todoTask}
+
+                  <button
+                    className="remove-btn"
+                    onClick={() => {
+                      removeTask(item.todoTask);
+                    }}
+                  >
+                    Remove
+                    <GoChecklist />
+                  </button>
+                </div>
+              );
+            })
+          ) : (
+            <div className="no-task">
+              <h3 className="empty">No Task Yet!</h3>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default ToDo;
